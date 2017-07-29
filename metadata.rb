9927f87f6e0ead6240d9d7a951b9bdb8195f1b0f
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-07-29 16:35:30 +0400

    Update config template

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'themis-finals'
 description 'Installs and configures Themis Finals'
-version '1.3.4'
+version '1.4.0'
 
 recipe 'themis-finals', 'Installs and configures Themis Finals'
 depends 'git', '~> 6.0.0'
